d13e92e222e29f2b3a64a82cbbba9072b9b01d90
painttt <vpaint345@gmail.com>
2024-08-05 11:43:08 +0700

    painttt
    com2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import './style.css'
+import Calendar from './class_compon';
+
+
+
+
 function App(){
   return(
     <>
     <h2>DDDDDDeveloperThai.com</h2>
     <div className='title'>React and React Native</div>
+    <h1><center><Calendar/></center></h1>
   </>
   )
   
