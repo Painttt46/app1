3c009735c5219cd6909a8673c34eeb119dd7869d
painttt <vpaint345@gmail.com>
2024-08-05 10:32:26 +0700

    Initialize project using Create React App
    first

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './style.css'
+function App(){
+  return(
+    <>
+    <h2>DDDDDDeveloperThai.com</h2>
+    <div className='title'>React and React Native</div>
+  </>
+  )
+  
+}
+export default App;
