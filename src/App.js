63d0ab450976e3ae80d85ef5c00af39106574916
Painttt46 <156067688+Painttt46@users.noreply.github.com>
2024-09-02 11:33:18 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react'
-import RefsFunc from './refs_gunc'
-import MassageBox from './state_func'
+import { userContext } from './context'
+import Header2 from './context-header2'
+import Content2 from './context-content2'
 
 
 
 
 export default function App(){
+  let [user,setUser]=React.useState('')
   return(
-    //<RefsFunc />
-    <MassageBox />
+    <userContext.Provider value={[user,setUser]}>
+      <Header2/>
+      <Content2/>
+    </userContext.Provider>
+    
   )
 }
